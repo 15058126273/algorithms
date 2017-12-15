fa2e60fc04432e8f7ec3838225771e401723c4b5
yjy <1335823608@qq.com>
2017-12-15 18:26:27 +0800

    1.获取峰值 2.字符串转整数

--- src/common/exercise/Peak.java
+++ src/common/exercise/Peak.java
@@ -1,9 +1,21 @@
-package common.math.array;
+package common.exercise;
 
 import java.util.Arrays;
 
 /**
- * 求数组中的峰值
+ * @description: 取峰值
+ * 给出一个整数数组（size 为 n），其具有以下特点：
+    1、相邻位置的数字是不同的
+    2、A[0] < A[1] 并且 A[n - 2] > A[n - 1]
+    假定P是峰值的位置则满足 A[P] > A[P-1] 且 A[P] > A[P+1]，写一个函数返回数组中所有峰值的位置。
+    格式：
+    输入行输入一个整数数组，输出行输出所有数组中的峰值的位置。
+    样例输入
+    [ 1，2，1，3，4，5，7，6 ]
+    样例输出
+    1，6
+ * @author: yjy
+ * @datetime: 2017/12/15 14:45
  */
 public class Peak {
 
@@ -25,7 +37,7 @@ public class Peak {
      * @param arr 给定数组
      * @return 高峰下标数组
      */
-    private static int[] findBigPeak(int[] arr) {
+    public static int[] findBigPeak(int[] arr) {
         int[] index;
         int len = arr.length;
         if (len < 3) {
@@ -48,7 +60,7 @@ public class Peak {
      * @param arr 给定数组
      * @return 低峰下标数组
      */
-    private static int[] findSmallPeak(int[] arr) {
+    public static int[] findSmallPeak(int[] arr) {
         int[] index;
         int len = arr.length;
         if (len < 3) {
@@ -71,7 +83,7 @@ public class Peak {
      * @param arr 给定数组
      * @return 所有峰值下标数组
      */
-    private static int[] findAllPeak(int[] arr) {
+    public static int[] findAllPeak(int[] arr) {
         int[] index;
         int len = arr.length;
         if (len < 3) {
---
+++ src/common/exercise/StringToInt.java
@@ -0,0 +1,71 @@
+package common.exercise;
+
+/**
+ * @description:
+ * 实现atoi这个函数，将一个字符串转换为整数。如果没有合法的整数，返回0。
+ * 若整数超出了32位整数的范围，如果是正整数则返回 INT_MAX（2147483647），或者如果是负整数则返回 INT_MIN（-2147483648）。
+    格式：
+    输入行每一行输入一个字符串，最后每一行输出经过转换后得到的整数。
+    样例输入
+    "10"
+    "-1"
+    "123123123123123"
+    "1.0"
+    样例输出
+    10
+    -1
+    2147483647
+    1
+ * @author: yjy
+ * @datetime: 2017/12/15 14:49
+ */
+public class StringToInt {
+
+    public static void main(String[] args) {
+        String a = "-1215514";
+        System.out.println(atoi(a));
+    }
+
+    /**
+     * 将字符串转为整数
+     * @param a 字符串
+     * @return 整数
+     */
+    public static int atoi(String a) {
+        int count = 0, len;
+        if (a == null || (len = a.length()) == 0) {
+            return count;
+        }
+        char[] chars = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        char ac;
+
+        for (int i = len - 1, pCount = 0, lastP = len; i >= 0; i --) {
+            if ((ac = a.charAt(i)) == '-') {
+                if (i == 0) {
+                    return count * -1;
+                } else {
+                    return 0;
+                }
+            } else if (ac == '.') {
+                if (++pCount > 1 || count != 0) {
+                    return 0;
+                } else {
+                    lastP = i;
+                    continue;
+                }
+            }
+            boolean matched = false;
+            for (int c = 0; c < chars.length; c ++) {
+                if ((matched = chars[c] == ac)) {
+                   count += Math.pow(10, lastP - i - 1) * c;
+                   break;
+                }
+            }
+            if (!matched) {
+                return 0;
+            }
+        }
+        return count;
+    }
+
+}
