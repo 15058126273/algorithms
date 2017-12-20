6a90fef0a82bb4802333d04bed5fbba770c73bb0
yjy <1335823608@qq.com>
2017-12-20 17:44:16 +0800

    1.更换命名

--- src/common/math/Sqrt.java
+++ src/common/math/Sqrt.java
@@ -1,27 +1,27 @@
 package common.math;
 
 public class Sqrt {
-	
-	public static void main(String[] args) {
-		int a = 12;
-		printCallStatck(a);
-	}
 
-	 public static void printCallStatck(int a) {
-	    	int count = 0;
-	    	long time1 = System.nanoTime();
-	    	double c = 0;
-	    	double t = 1e-15;
-	    	while ( (c = Math.pow(t, 2)) < a - (1e-5) || c > a + (1e-5)) {
-	    		t = (t + a/t) / 2;
-//	    		count++;
-//	    		System.out.println(t);
-	    	}
-	    	long time2 = System.nanoTime();
-	    	double d = Math.sqrt(a);
-	    	long time3 = System.nanoTime();
-	    	System.out.println("time:" + (time2 - time1) + ",t:" +t);
-	    	System.out.println("time:" + (time3 - time2) + ",t:" + d);
-	    	
-	    }
+    public static void main(String[] args) {
+        int a = 12;
+        sqrt(a);
+    }
+
+    public static void sqrt(int a) {
+        long time0 = System.nanoTime();
+        long time1 = System.nanoTime();
+        double c;
+        double t = 1e-15;
+        while ((c = Math.pow(t, 2)) < a - (1e-5) || c > a + (1e-5)) {
+            t = (t + a / t) / 2;
+        }
+        long time2 = System.nanoTime();
+        double d = Math.sqrt(a);
+
+        long time3 = System.nanoTime();
+        System.out.println("time:" + (time1 - time0));
+        System.out.println("time:" + (time2 - time1) + ",t:" + t);
+        System.out.println("time:" + (time3 - time2) + ",t:" + d);
+
+    }
 }
